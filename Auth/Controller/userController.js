fea1bf740410c894b62112534644ebb4b20c2682
manojyadav <manoj1052000@gmail.com>
2025-01-13 01:14:17 +0530

    thirds time commit for the remove login and registerpage

--- Auth/Controller/userController.js
+++ Auth/Controller/userController.js
@@ -2,44 +2,6 @@ const User = require('./../model/userModel');
 const bcrypt = require('bcrypt');
 
 
-const register = async (req,res)=>{
-    const {fullName,email,password,phone,gender} =req.body;
-    try{
-        const user = new User({fullName,email,password,phone,gender});
-        await user.save();
-        res.json({message:"user Registerd Successfully",status:true,user:user});
-    }catch(error){
-        console.error(error);
-        res.status(500).json({message:"Internal Server Issue",status:false});
-    }
-};
-
-// login
-// login
-const login = async (req,res)=>{
-    const {email,password} =req.body;
-    try{
-        const userData = await User.findOne({email});
-        if(!userData) return res.json(userData,{message:"Invalid Email Address"});
-
-        // compare the password 
-        const comparePassword = await bcrypt.compare(password, userData.password);
-        if(!comparePassword) return res.json({message:"Invalid Password"});
-
-        // token created 
-        const token = await jwt.sign({id:userData._id,email:userData.email,phone:userData.phone},SECRETE_KEY,{expiresIn:'1h'});
-        console.log("Token:", token);
-        // cookies 
-        res.cookie('token', token, {httpOnly: true });
-        // res.cookies('token',token);
-        res.json({message:"user login Successfully ",status:true,data:userData,token});
-    }catch(error){
-        console.log(error);
-        res.status(500).json({message:"Internal Srver Issue",status:false,data:null})
-    }
-}
-
-
 // getAll 
 const getAllData = async (req,res)=>{
     try{
@@ -56,7 +18,6 @@ const getAllData = async (req,res)=>{
 };
 
 module.exports ={
-    register,
-    login,
+
     getAllData,
 }
--- Auth/Router/userController.js
+++ Auth/Router/userController.js
@@ -1,10 +1,8 @@
 const express = require('express');
-const { register, login, getAllData } = require('../Controller/userController');
+const {  getAllData } = require('../Controller/userController');
 const router = express.Router();
 
 
-router.post('/register', register);
-router.post('/login',login);
 router.get('/getAll',getAllData);
 
 
